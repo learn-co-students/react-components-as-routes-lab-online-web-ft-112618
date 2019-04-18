f4a8944713f7cbee00fc96900c3f2a570cd96297
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-04-17 20:37:21 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,17 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
-  );
-}
+      <h1>Directors Page</h1>
+        {directors.map((director) => (
+          <div>
+            <h4>{director.name}</h4>
+            {director.movies.map((movie) => (
+              <p> * {movie}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  };
 
 export default Directors
